1d42fdf1aeecc559a674163d4b36ee7c8b69f067
nepoz <bishrutbhattarai@gmail.com>
2021-08-17 15:15:46 -0500

    Added click listeners for coloring boxes

--- color_my_views/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ color_my_views/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners() {
+        val boxes = listOf<View>(
+            findViewById(R.id.box_one_text),
+            findViewById(R.id.box_two_text),
+            findViewById(R.id.box_three_text),
+            findViewById(R.id.box_four_text),
+            findViewById(R.id.box_five_text),
+            findViewById(R.id.root_constraint)
+        )
+
+        boxes.forEach {
+            it.setOnClickListener { view -> makeColored(view) }
+        }
+    }
+
 }
